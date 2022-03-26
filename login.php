9156a0788fbf0f5f5cbd852512edb0d603fa67b8
mfrag2025 <59969280+mfrag2025@users.noreply.github.com>
2022-03-26 04:55:06 -0400

    Update login.php

--- login.php
+++ login.php
@@ -22,14 +22,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter username.";
     } else{
-        $username = trim($_POST["username"]);
+        $username = htmlspecialchars(trim($_POST["username"]));
+        //$username = trim($_POST["username"]);
     }
     
     // Check if password is empty
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter your password.";
     } else{
-        $password = trim($_POST["password"]);
+        $password = htmlspecialchars(trim($_POST["password"]));
+        //$password = trim($_POST["password"]);
     }
     
     // Validate credentials
